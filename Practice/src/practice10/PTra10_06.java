ca947794802a13359373b0c552c6be3f20c2e37c
LAPTOP-FLPI0A24\tanah <asu.812@gmail.com>
2018-06-20 18:20:19 +0900

    PTra11_02まで完了

--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -18,6 +18,41 @@ public class PTra10_06 {
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
+		Car[] cars = new Car[3];
+
+		final int distance = 300;
+
+		Car car1 = new Car();
+		car1.color = "blue";
+		car1.gasoline = 50;
+
+		Car car2 = new Car();
+		car2.color = "black";
+		car2.gasoline = 80;
+
+		Car car3 = new Car();
+		car3.color = "pink";
+		car3.gasoline = 60;
+
+		cars[0] = car1;
+		cars[1] = car2;
+		cars[2] = car3;
+
+		for (int i = 0; i < cars.length; i++) {
+
+			int sum = 0;
+			int n = 0;
+			while (sum <= distance) {
+				n++;
+				sum += cars[i].run();
+				if (cars[i].gasoline < 0) {
+					System.out.println("目的地に到達できませんでした");
+					return;
+				}
+			}
+			System.out.println("目的地にまで" + n + "時間かかりました。残りのガソリンは、" + cars[i].gasoline + "リットルです");
+
+		}
 
 	}
 }
--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -6,15 +6,26 @@ package practice11;
  * Copyright(c) Rhizome Inc. All Rights Reserved.
  */
 
+
+
 public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
 
+		Book[] book = FileReaderClass.readBookDataFile();
+		//int型配列の中にはintのインスタンス,String型配列の中にはStringのインスタンス,
+		//book型配列の中にはbookのインスタンスが入っている。
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String input = new java.util.Scanner(System.in).nextLine();
 
-		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		for (int i = 0; i < book.length; i++) {
+			if(book[i].title.contains(input)) {
+				// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+				System.out.println(book[i].dispBookInfo());
+			}
+		}
 
 	}
 }
