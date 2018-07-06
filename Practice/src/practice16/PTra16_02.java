0adfb3562328f16a999befc9fefd11c33bfad707
LAPTOP-FLPI0A24\tanah <asu.812@gmail.com>
2018-07-06 18:50:21 +0900

    practice18完了

--- Practice/src/practice16/PTra16_02.java
+++ Practice/src/practice16/PTra16_02.java
@@ -10,13 +10,36 @@ public class PTra16_02 {
 	public static void main(String[] args) {
 
 		boolean b = true;
+		Boolean ob = new Boolean(b);
+		System.out.println(ob);
+
 		byte by = 100;
+		Byte oby = new Byte(by);
+		System.out.println(oby);
+
 		short s = 300;
+		Short os = new Short(s);
+		System.out.println(os);
+
 		char c = '日';
+		Character oc = new Character(c);
+		System.out.println(oc);
+
 		int i = 1000;
+		Integer oi = new Integer(i);
+		System.out.println(oi);
+
 		long l = 2000L;
+		Long ol = new Long(l);
+		System.out.println(ol);
+
 		float f = 3.14F;
+		Float of = new Float(f);
+		System.out.println(of);
+
 		double d = 453.592;
+		Double od = new Double(d);
+		System.out.println(od);
 
 		// ★ 上記基本型を、それぞれのラッパークラスに代入してください
 	}
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,70 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+        ArrayList<Player> array = new ArrayList<Player>();
 
+        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+            	String[] list = line.split(",",0);
+
+            	Player player = new Player();
+
+            	player.setPosition(list[0]);
+            	player.setName(list[1]);
+            	player.setCountry(list[2]);
+            	player.setTeam(list[3]);
+
+
+                // ★ 1行ごとにArrayListに格納してください
+                array.add(player);
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが見つかりません");
+        }
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+        Collections.shuffle(array);
+
+        int ct=0;
+        int ct2=0;
+        int ct3=0;
+        int ct4=0;
+
+        for(int i=0;i<array.size();i++) {
+        	if(array.get(i).getPosition().contains("GK")) {
+        		if(ct<1) {
+	        		System.out.println(array.get(i));
+	        		ct++;
+        		}
+
+        	}
+
+        	if(array.get(i).getPosition().contains("DF")) {
+        		if(ct2<4) {
+	        		System.out.println(array.get(i));
+	        		ct2++;
+        		}
+
+        	}
+
+        	if(array.get(i).getPosition().contains("MF")) {
+        		if(ct3<4) {
+	        		System.out.println(array.get(i));
+	        		ct3++;
+        		}
+
+        	}
+
+        	if(array.get(i).getPosition().contains("FW")) {
+        		if(ct4<2) {
+	        		System.out.println(array.get(i));
+	        		ct4++;
+        		}
+
+        	}
+        }
 
 	}
 }
